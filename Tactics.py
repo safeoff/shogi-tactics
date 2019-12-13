e0d6f09c3ade6b7a76930023b3964428b648eb8b
ry <sgmrrc@gmail.com>
2019-12-13 20:45:40 +0900

    悪手判定を関数化

--- Tactics.py
+++ Tactics.py
@@ -12,6 +12,36 @@ class Move:
 		self.move_eval = args[4]
 		self.bestmove_eval = args[5]
 
+
+# 悪手かどうかの判定
+def is_badmove(premove, move, bestmove):
+	# 指した手による機会損失（最善手と指した手の評価値の差）を計算
+	# 評価値は先後で符号が反転しているのでここで調整する
+	premove_eval = -premove.pvs[0].eval
+	move_eval = -move.pvs[0].eval
+	bestmove_eval = bestmove.pvs[0].eval
+	lost = abs(bestmove_eval - move_eval)
+
+	# 疑問手含む問題ない手：採用しない
+	if lost < 1000:
+		return False
+	# 形勢が極端に傾いている局面での・・・
+	if abs(premove_eval > 2500):
+		# 悪手：採用しない
+		if (lost > 1000 and lost < 2000):
+			return False
+		# 詰み逃し：採用しない　（逆転は採用）
+		if (abs(move_eval) > 9999 and premove_eval*bestmove_eval >= 0):
+			return False
+	# 読み筋が少ない：採用しない
+	splited_pv = bestmove.pvs[0].pv
+	if len(splited_pv.split()) < 3:
+		return False
+
+	# 上記以外は悪手・頓死・詰み逃し・必死逃しと判定　採用
+	return True
+
+
 # 悪手を指した局面を抽出
 def choice_badmove(think_results):
 	badmoves = []
@@ -26,37 +56,20 @@ def choice_badmove(think_results):
 
 		# 指した手による機会損失（最善手と指した手の評価値の差）を計算
 		# 評価値は先後で符号が反転しているのでここで調整する
-		premove_eval = -premove_eval
-		move_eval = -think_results[i+1][2].pvs[0].eval
-		bestmove_eval = item[2].pvs[0].eval
-		# 先手番なら反転
-		if i%2 == 0:
-			premove_eval = -premove_eval
-			move_eval = -move_eval
-			bestmove_eval = -bestmove_eval
 
-		lost = abs(bestmove_eval - move_eval)
-		# 疑問手含む問題ない手：採用しない
-		if lost < 1000:
-			continue
-		# 形勢が極端に傾いている局面での・・・
-		if abs(premove_eval > 2500):
-			# 悪手：採用しない
-			if (lost > 1000 and lost < 2000):
-				continue
-			# 詰み逃し：採用しない　（逆転は採用）
-			if (abs(move_eval) > 9999 and premove_eval*bestmove_eval >= 0):
-				continue
-		# 読み筋が少ない：採用しない
-		bestmove = item[2].pvs[0].pv
-		if len(bestmove.split()) < 3:
-			continue
-
-		# 上記以外は悪手・頓死・詰み逃し・必死逃しと判定　採用
-		premove = item[0]
-		move = item[1] + " " + think_results[i+1][2].pvs[0].pv
-		badmove = Move(i+2, premove, move, bestmove, move_eval, bestmove_eval)
-		badmoves.append(badmove)
+		# 悪手なら保存
+		if is_badmove(think_results[i-1][2], think_results[i+1][2], item[2] ):
+			move_eval = -think_results[i+1][2].pvs[0].eval
+			bestmove_eval = item[2].pvs[0].eval
+			# 先手番なら反転
+			if i%2 == 0:
+				move_eval = -move_eval
+				bestmove_eval = -bestmove_eval
+			bestmove = item[2].pvs[0].pv
+			premove = item[0]
+			move = item[1] + " " + think_results[i+1][2].pvs[0].pv
+			badmove = Move(i+2, premove, move, bestmove, move_eval, bestmove_eval)
+			badmoves.append(badmove)
 
 	return badmoves
 
@@ -102,17 +115,18 @@ def create_tactics(battle_type, moves):
 	badmoves = choice_badmove(think_results)
 
 	# 駒の動きを日本語に変換
-	tactics = {}
+	tactics = []
 	for badmove in badmoves:
-		tactics.update([
+		map = {
 			# TODO SFENの局面がまだ
-			("board", "未実装"),
-			("bestmove", convert_words(badmove.bestmove)),
-			("bestmove_eval",badmove.bestmove_eval),
-			("move", convert_words(badmove.move)),
-			("move_eval", badmove.bestmove_eval),
-			("premove", badmove.premove),
-			("battle_type", battle_type)
-		])
+			"board": "未実装",
+			"bestmove": convert_words(badmove.bestmove),
+			"bestmove_eval": badmove.bestmove_eval,
+			"move": convert_words(badmove.move),
+			"move_eval": badmove.move_eval,
+			"premove": badmove.premove,
+			"battle_type": battle_type
+		}
+		tactics.append(map)
 
 	print(tactics)
