a01f06fad50aa33cdbfcc4f9bc52a3d4c8f91d2f
ry <sgmrrc@gmail.com>
2019-12-15 04:57:20 +0900

    計算結果をAnki形式に変更

--- create.py
+++ create.py
@@ -1,5 +1,6 @@
 import sys
 import collections
+import urllib
 sys.path.append('../kifu-downloader')
 import kifuDownloader
 sys.path.append('../csa2moves')
@@ -9,8 +10,45 @@ import AddAnki
 import Tactics
 
 
+# 一つ前の手を強調表示するための数字を得る　sfenreader用
+def calc_move_num(move):
+	nummap = {"一": 1, "二": 2, "三": 3, "四": 4, "五": 5, "六": 6, "七": 7, "八": 8, "九": 9}
+	chars = list(move)
+	num = chars[1] + str(nummap[chars[2]])
+	return num
+
+
+# sfen文字列を、局面のpng画像URLに変換
+def convert_board(sfen, premove):
+	api = "http://sfenreader.appspot.com/sfen"
+	sfen = "?sfen=" + urllib.parse.quote(tactics["sfen"])
+	lm = "&lm=" + calc_move_num(premove)
+	board = "<img src=\'" + api + sfen + lm + "\'>"
+
+	return board
+
+
+# 計算結果をAnki形式に変換
+# front: 局面、戦法名、一つ前の手、最善手の評価値、本譜の評価値
+# back: 最善手の読み筋、本譜の読み筋
+def convert_tactics(tactics):
+	board = convert_board(tactics["sfen"], tactics["premove"])
+	battle_type = tactics["battle_type"] + "　"
+	premove = tactics["premove"] + "まで　"
+	bestmove_eval = "最善手の評価値：　" + str(tactics["bestmove_eval"]) + "　"
+	move_eval = "本譜の評価値：　" + str(tactics["move_eval"]) + "　"
+
+	front = board + battle_type + premove + bestmove_eval + move_eval
+
+	bestmove = "最善手+CPUの読み筋：　" + tactics["bestmove"] + "　"
+	move = "本譜+CPUの読み筋：　" + tactics["move"] + "　"
+
+	back = bestmove + move
+	return front, back
+
+
+# ウォーズの棋譜を取得する（10分切れ）
 if __name__ == "__main__":
-	# ウォーズの棋譜を取得する（10分切れ）
 	id = "safeoff"
 	gt = ""
 	kifus = kifuDownloader.download_warskifu(id, gt)
@@ -28,9 +66,8 @@ if __name__ == "__main__":
 	# Ankiにカードを登録
 	battle_types = []
 	for tactics in tacticss:
-		front = tactics["board"] + tactics["battle_type"] + tactics["premove"]
-		back = tactics["move"] + str(tactics["move_eval"]) + tactics["bestmove"] + str(tactics["bestmove_eval"])
-		deck = "学習::将棋::実践次の一手"
+		front, back = convert_tactics(tactics)
+		deck = "学習::将棋::実践次の一手::" + tactics["battle_type"]
 
 		anki_result = AddAnki.addAnki(front, back, deck)
 
